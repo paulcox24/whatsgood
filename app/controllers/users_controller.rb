10844558211c55ca5ad6e7b225e1a484b66e56ae
Paul Cox <paulcox24@gmail.com>
2014-09-29 21:01:37 -0600

    Ran migration added other columns to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def profile
+    @user = current_user
   end
 
   def user_events
---
+++ db/migrate/20140930025913_add_fields_to_users.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :location, :string
+    add_column :users, :description, :text
+  end
+end
