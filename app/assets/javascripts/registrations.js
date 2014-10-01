95643440945d615755e2e298dd74fc59b9cfab59
Paul Cox <paulcox24@gmail.com>
2014-09-30 22:58:42 -0600

    Lat and long forms hidden, Addres populates with use current location

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -1,24 +1,39 @@
-var options = {
-  enableHighAccuracy: true,
-  maximumAge: 0
-};
+$(document).ready(function() {
 
-function success(pos) {
-  var coord = pos.coords;
-  $('#user_longitude').val(coord.longitude);
-  $('#user_latitude').val(coord.latitude);
-  $('#user_address').val();
+  var options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  };
 
-  // $('.location-data').html('Your current position is:<br>' + 'Latitude : ' + crd.latitude +'<br>Longitude: ' + crd.longitude + '<br>More or less ' + crd.accuracy + ' meters.');
-};
+  function success(pos) {
+    var coord = pos.coords;
+    geocoder = new google.maps.Geocoder();
+    $('#user_longitude').val(coord.longitude);
+    $('#user_latitude').val(coord.latitude);
+
+    var latlng = new google.maps.LatLng(coord.latitude,coord.longitude);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          $('#user_address').val(results[0].formatted_address);
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+    
+
+    // $('.location-data').html('Your current position is:<br>' + 'Latitude : ' + crd.latitude +'<br>Longitude: ' + crd.longitude + '<br>More or less ' + crd.accuracy + ' meters.');
+  };
 
 
-function error(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
-};
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
 
 
-$('#location_button').click(function(){
-  navigator.geolocation.getCurrentPosition(success, error, options);
-  console.log('this worked');
+  $('#location_button').click(function(){
+    navigator.geolocation.getCurrentPosition(success, error, options);
+    console.log('this worked');
+  });
 });
