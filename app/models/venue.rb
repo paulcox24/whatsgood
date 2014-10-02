eab4297f6bc446080520a9be33cd7a436c6b2b1c
djmrr <mikedjrussell@gmail.com>
2014-10-02 00:56:40 -0600

    added how things were geocoded in venue.rb model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,10 @@ class Venue < ActiveRecord::Base
   has_many :events
   has_many :venue_acts
   has_many :acts, through: :venue_acts
+  # adding geocoder stuff ##
+  geocoded_by :location   # can also be an IP address
+  after_validation :geocode, :if => :location_changed?         # auto-fetch coordinates
+
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
 end
