c1d32a02f2ed824e0535c07e8266546f5fcd6e07
Paul Cox <paulcox24@gmail.com>
2014-10-02 09:19:29 -0600

    Cleaned up latlong syntax

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   
   def home
     if current_user
-    	get_eventful(current_user.latitude, current_user.longitude)
+      latlong = "#{current_user.latitude},#{current_user.longitude}"
+    	get_eventful(latlong)
     else
       get_eventful('Salt Lake City')
     end  
@@ -14,9 +15,9 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
-  def get_eventful(latitude, longitude=nil)
+  def get_eventful(latlong)
     eventful = Eventful::API.new ENV["EVENTFUL_API_KEY"]
-    result = eventful.call 'events/search', :category => 'music', :location => "#{latitude},#{longitude}", :within => 5, :date => 'Future', :image_sizes => 'large', :sort_order => 'popularity'
+    result = eventful.call 'events/search', :category => 'music', :location => latlong, :within => 5, :date => 'Future', :image_sizes => 'large', :sort_order => 'popularity'
     @events = result['events']['event']
   end
 end
