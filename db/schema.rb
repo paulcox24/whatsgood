9f0a534b682a4ec4b55f2cdfadc5d9a23561c639
djmrr <mikedjrussell@gmail.com>
2014-10-02 00:51:46 -0600

    added latitude and longitude floats to Venue model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141001022017) do
+ActiveRecord::Schema.define(version: 20141002065104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,14 @@ ActiveRecord::Schema.define(version: 20141001022017) do
 
   add_index "interests", ["user_id"], name: "index_interests_on_user_id", using: :btree
 
+  create_table "locations", force: true do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email",                                           default: "", null: false
     t.string   "encrypted_password",                              default: "", null: false
@@ -104,6 +112,8 @@ ActiveRecord::Schema.define(version: 20141001022017) do
     t.string   "website"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
