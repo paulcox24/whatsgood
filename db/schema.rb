d6651a99267d0feb790ef68849726c10cf7fc151
djmrr <mikedjrussell@gmail.com>
2014-10-02 14:13:36 -0600

    readded the address to venue via migration...this time it worked

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002200506) do
+ActiveRecord::Schema.define(version: 20141002201046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20141002200506) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "genre"
+    t.string   "address"
   end
 
 end
