6c8083f82f6aed2958b40f31e1af6325a7fba9f1
Paul Cox <paulcox24@gmail.com>
2014-10-02 09:15:36 -0600

    Sort by popularity

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
-  include StaticPagesHelper
   
   def home
     if current_user
@@ -13,5 +12,11 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
+
+  def get_eventful(latitude, longitude=nil)
+    eventful = Eventful::API.new ENV["EVENTFUL_API_KEY"]
+    result = eventful.call 'events/search', :category => 'music', :location => "#{latitude},#{longitude}", :within => 5, :date => 'Future', :image_sizes => 'large', :sort_order => 'popularity'
+    @events = result['events']['event']
   end
 end
