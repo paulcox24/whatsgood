8e90f0e7a1a842be342518420f773551801368ff
djmrr <mikedjrussell@gmail.com>
2014-10-02 01:00:24 -0600

    realized genre still was erroring because it wasnt added to model[fixed][added to model]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002065104) do
+ActiveRecord::Schema.define(version: 20141002065927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20141002065104) do
     t.datetime "updated_at"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "genre"
   end
 
 end
