dac1f5ebf28c4a657f14f657c1957d239466a64a
Paul Cox <paulcox24@gmail.com>
2014-10-04 15:53:17 -0600

    Usersga.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,12 @@ class StaticPagesController < ApplicationController
   def home
     if current_user
       latlong = "#{current_user.latitude},#{current_user.longitude}"
-    	get_eventful(latlong)
+      categories = current_user.categories.collect { |category| category.name }.join(',')
+    	get_eventful(latlong, categories)
     else
-      get_eventful('Salt Lake City')
+      default_city = 'Salt Lake City'
+      default_categories = 'music,comedy,sports'
+      get_eventful(default_city, default_categories)
     end  
   end
 
@@ -15,9 +18,16 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
-  def get_eventful(latlong)
+  def get_eventful(latlong, categories=nil)
     eventful = Eventful::API.new ENV["EVENTFUL_API_KEY"]
-    @result = eventful.call 'events/search', :category => 'music', :location => latlong, :within => 5, :date => 'Future', :image_sizes => 'large', :sort_order => 'popularity', :page_size => 20
+    @result = eventful.call 'events/search',
+              :category => categories,
+              :location => latlong,
+              :within => 5,
+              :date => 'Future',
+              :image_sizes => 'large',
+              :sort_order => 'popularity',
+              :page_size => 20
     @events = @result['events']['event']
   end
 end
