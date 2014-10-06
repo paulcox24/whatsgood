dce9634a89f1f5fedcf6152fe15eee3fceff2b32
derekdapp <derekdapp@gmail.com>
2014-10-06 15:41:10 -0600

    shuffle.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,5 @@
 //= require_tree .
 //= require underscore
 //= require gmaps/google
+//= require jquery.shuffle
+
---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  var $grid = $('#grid'),
+      $sizer = $grid.find('.shuffle__sizer');
+
+  $grid.shuffle({
+    itemSelector: '.picture-item',
+    sizer: $sizer
+  });
+});
