5144d3d83220fe995af799b82326770d4e3fc4e0
Paul Cox <paulcox24@gmail.com>
2014-10-06 15:38:55 -0600

    Merged mikes maps and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -111,7 +111,7 @@ ActiveRecord::Schema.define(version: 20141002204736) do
   add_index "venue_acts", ["venue_id"], name: "index_venue_acts_on_venue_id", using: :btree
 
   create_table "venues", force: true do |t|
-    t.text     "location"
+    t.text     "address"
     t.string   "name"
     t.text     "description"
     t.string   "venue_type"
@@ -123,7 +123,6 @@ ActiveRecord::Schema.define(version: 20141002204736) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "genre"
-    t.string   "address"
   end
 
 end
