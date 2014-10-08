26af7b86d6779d2852750e19f0a2400ccdaa9c12
Paul Cox <paulcox24@gmail.com>
2014-10-08 17:09:22 -0600

    takes data from card and populates new form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,7 @@ class EventsController < ApplicationController
   def new
     @event = Event.new(name: params[:name],
                        description: params[:description],
-                       image: params[:image],
+                       image_url: params[:image_url],
                        start_time: params[:start_time],
                        stop_time: params[:stop_time],
                        venue_address: params[:venue_address],
@@ -47,7 +47,7 @@ class EventsController < ApplicationController
 
   private
   def event_params
-    params.require(:event).permit(:start_time, :stop_time, :venue_address, :venue_name, :venue_address, :city_name, :image, :description, :category, :genre, :name, :rating, :website)
+    params.require(:event).permit(:start_time, :stop_time, :venue_address, :venue_name, :venue_address, :city_name, :image_url, :description, :category, :genre, :name, :rating, :website)
   end
 
   def set_event
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008221010) do
+ActiveRecord::Schema.define(version: 20141008225504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,10 +62,10 @@ ActiveRecord::Schema.define(version: 20141008221010) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "stop_time"
-    t.binary   "image"
     t.string   "venue_name"
     t.text     "venue_address"
     t.string   "city_name"
+    t.string   "image_url"
   end
 
   add_index "events", ["venue_id"], name: "index_events_on_venue_id", using: :btree
