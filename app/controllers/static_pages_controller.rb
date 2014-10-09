92d64ca5c7fb00d7e7525cccef10145d1ece3efd
Paul Cox <paulcox24@gmail.com>
2014-10-08 19:40:58 -0600

    Cleaned up code in static_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,11 @@
 class StaticPagesController < ApplicationController
   
   def home
-    default_date = 'Future'
+    date = 'Future'
     if current_user
-      latlong = "#{current_user.latitude},#{current_user.longitude}"
-      categories = current_user.categories.collect { |category| category.name }.join(',')
-    	get_eventful(latlong, categories, default_date)
+      user_eventful(date)
     else
-      default_city = 'Salt Lake City'
-      default_categories = 'music,comedy,sports'
-      get_eventful(default_city, default_categories, default_date)
+      default_eventful(date)
     end 
     make_map(@events)
   end
@@ -34,13 +30,9 @@ class StaticPagesController < ApplicationController
   def today
     date = 'Today'
     if current_user
-      latlong = "#{current_user.latitude},#{current_user.longitude}"
-      categories = current_user.categories.collect { |category| category.name }.join(',')
-      get_eventful(latlong, categories, date)
+      user_eventful(date)
     else
-      default_city = 'Salt Lake City'
-      default_categories = 'music,comedy,sports'
-      get_eventful(default_city, default_categories, date)
+      default_eventful(date)
     end 
     make_map(@events)
   end
@@ -48,16 +40,14 @@ class StaticPagesController < ApplicationController
   def this_week
     date = 'This Week'
     if current_user
-      latlong = "#{current_user.latitude},#{current_user.longitude}"
-      categories = current_user.categories.collect { |category| category.name }.join(',')
-      get_eventful(latlong, categories, date)
+      user_eventful(date)
     else
-      default_city = 'Salt Lake City'
-      default_categories = 'music,comedy,sports'
-      get_eventful(default_city, default_categories, date)
+      default_eventful(date)
     end 
     make_map(@events)
   end  
+
+  private
   
   def get_eventful(latlong, categories=nil, date=nil)
     eventful = Eventful::API.new ENV["EVENTFUL_API_KEY"]
@@ -78,6 +68,18 @@ class StaticPagesController < ApplicationController
       marker.lng event['longitude']
       marker.infowindow "<h6><a style=padding: 1.25em; href=#{event['url']}>Event Link</a><br>Title: #{event['title']}<br>Venue: #{event['venue_name']}</h6>"
     end 
+  end
+
+  def default_eventful(date)
+    default_city = 'Las Vegas'
+    default_categories = 'music,comedy,sports'
+    get_eventful(default_city, default_categories, date)
+  end
+
+  def user_eventful(date)
+    latlong = "#{current_user.latitude},#{current_user.longitude}"
+    categories = current_user.categories.collect { |category| category.name }.join(',')
+    get_eventful(latlong, categories, date)
   end
 
 end
