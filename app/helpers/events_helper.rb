6d29aaca1bcf89250f41f1b17a0ad6593b528b3a
derekdapp <derekdapp@gmail.com>
2014-10-16 16:37:32 -0600

    cleaned up and fixed bug with cards stacking

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,14 +1,2 @@
 module EventsHelper
-	 def category_class(event)
-  	e = event
-  	category = ""
-  	e['categories']['category'].each do |i|
-      i.each do |j,k|
-        category = event['categories']['category']['id'] if !k 
-        category = "#{category} #{k} " if j == "id" 
-      end 
-    end 
-    puts category
-  return category
-  end
 end
