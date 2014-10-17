b83cc5c7a2b54134b6dc0faa52214b0c9ca2ea54
derekdapp <derekdapp@gmail.com>
2014-10-17 14:29:07 -0600

    added instruction from railscast

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :events
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   geocoded_by :address   # can also be an IP address
