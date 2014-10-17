efd524e8798e562bafbf5a77ab743fcdc322a2b5
Paul Cox <paulcox24@gmail.com>
2014-10-17 13:32:43 -0600

    Ajax follow action

--- app/controllers/socialization_controller.rb
+++ app/controllers/socialization_controller.rb
@@ -1,10 +1,27 @@
 class SocializationController < ApplicationController
 
   def follow
-    current_user.follow!(User.find(params['user']))
+    set_user
+    current_user.follow!(@user)
+
+    respond_to do |format|
+        format.html { redirect_to user_profile_path(@user) }
+        format.js
+      end
   end
 
   def unfollow
-    current_user.unfollow!(User.find(params['user']))
+    set_user
+    current_user.unfollow!(@user)
+
+    respond_to do |format|
+        format.html { redirect_to user_profile_path(@user) }
+        format.js
+      end
+  end
+
+  private
+  def set_user
+    @user = User.find(params['user'])
   end
 end
