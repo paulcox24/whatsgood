964014fb792859515b778dbe7661dcd77ae0915a
derekdapp <derekdapp@gmail.com>
2014-10-17 18:53:28 -0600

    modifying model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,28 @@ class User < ActiveRecord::Base
   def can_edit_user?(user)
     self == user || self.is_admin
   end  
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider 
+      user.uid      = auth.uid
+      user.first_name     = auth.info.name
+      user.save
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end   
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
 end
