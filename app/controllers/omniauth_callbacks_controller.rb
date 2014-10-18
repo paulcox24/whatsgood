6b778c2eaec8623a8dca9836a36a9a79a2f53ba8
derekdapp <derekdapp@gmail.com>
2014-10-17 18:13:26 -0600

    changes to controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,2 +1,7 @@
-class OmniauthCallbacksController < ApplicationController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+	def all
+    user = User.from_omniauth(request.env["omniauth.auth"])
+
+	end
+  alias_method :facebook, :all
 end
