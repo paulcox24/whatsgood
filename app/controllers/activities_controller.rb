028465ed6007db318ab9bbaf6365a2bb7c44cf98
Paul Cox <paulcox24@gmail.com>
2014-10-21 11:42:17 -0600

    Made full news feed public

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    if current_user && current_user.is_admin
-      @activities = PublicActivity::Activity.order('created_at desc')
-    else 
-      redirect_to root_path, notice: "Not allowed" 
-    end 
+    @activities = PublicActivity::Activity.order('created_at desc')
   end
 end
