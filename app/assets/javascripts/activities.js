4f4e366e8774ce8bdead72adbe76d460b80aa607
derekdapp <derekdapp@gmail.com>
2014-10-24 14:30:43 -0600

    moved is on screen function to application

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $("#feed .page").infinitescroll({
-    navSelector: "nav.pagination",
-    nextSelector: "nav.pagination a[rel=next]",
-    itemSelector: "#feed tr.post"
-  });
+	jQuery(function($) {
+  $('#feed').bind('scroll', function() {
+
+    })
+});
 });
