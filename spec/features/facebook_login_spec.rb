87369b718050ec1a053c1f4ed50f68fd76794b8f
derekdapp <derekdapp@gmail.com>
2014-10-27 15:24:54 -0600

    facebook stuff not working

---
+++ spec/features/facebook_login_spec.rb
@@ -0,0 +1,9 @@
+# require "rails_helper"
+
+# feature "Login with FB" do
+#   scenario "not sent when fields are not complete" do
+#    visit new_user_session_path
+#    click_on("Sign In With Facebook", :match => :first)
+#   end
+  
+# end
---
+++ spec/helpers/facebook_registration.rb
@@ -0,0 +1,4 @@
+background do
+   set_omniauth()
+   click_link_or_button 'Sign up with Facebook'
+end
