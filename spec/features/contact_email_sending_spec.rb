dc1ece664e20e8aa9d19d2dd45cec98308ec390a
derekdapp <derekdapp@gmail.com>
2014-10-27 14:29:17 -0600

    removed the save and show

--- spec/features/contact_email_sending_spec.rb
+++ spec/features/contact_email_sending_spec.rb
@@ -1,13 +1,19 @@
 require "rails_helper"
 
 feature "Contact Email" do
-  scenario "Email is sent from contact page" do
-    
-
-    # Expect user to be logged in and see their email at the top of the page
-    expect(page.html).to have_link(user.email, user_path(user))
-
-    expect(page).to have_content("Sign Out")
-    expect(page).to have_content("This is the project one.")
+  scenario "not sent when fields are not complete" do
+    visit static_pages_contact_path
+    fill_in "email_name", :with => "Test Name"
+    click_button "Send"
+    expect(page).to have_content ("All Fields Must Be Complete")
+  end
+  scenario "sent when fields are complete" do
+    visit static_pages_contact_path
+    fill_in "email_name", :with => "Test Name"
+    fill_in "email_email", :with => "email@email.com"
+    fill_in "email_subject", :with => "Test Subject"
+    fill_in "email_message", :with => "Test Message"
+    click_button "Send"
+    expect(page).to have_content ("Message Sent")
   end
 end
