d0f73a9e3b5b58ddec54391eab343c6de4f5324b
djmrr <mikedjrussell@gmail.com>
2014-10-28 17:46:47 -0600

    adding some stuff, getting closer

--- app/assets/javascripts/devise_sessions_modal.js
+++ app/assets/javascripts/devise_sessions_modal.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$("#tragdor").click(function(){
 		$("#makeitso").addClass('say_welcome');
+		$(".say_welcome").append("<span id=welcome>welcome to whatsgood!</span>")
 	});
 });
