8573e2b20f71aa3980fa7184863941600e6a7bdf
Paul Cox <paulcox24@gmail.com>
2014-10-28 21:17:49 -0600

    merge with master

--- app/assets/javascripts/devise_sessions_modal.js
+++ app/assets/javascripts/devise_sessions_modal.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$("#tragdor").click(function(){
 		$("#makeitso").addClass('say_welcome');
-		$(".say_welcome").append("<span id=welcome>whatsgood!</span>")
+		$(".say_welcome").append("<span id=welcome>whatsgood</span>")
 		$( ".say_welcome" ).fadeOut( 3120, function() {
     		// Animation complete.
   		});
