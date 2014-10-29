ead7428edd1bfa6c091c9a4b5976758360b1c4d9
djmrr <mikedjrussell@gmail.com>
2014-10-28 18:41:23 -0600

    made it look decent

--- app/assets/javascripts/devise_sessions_modal.js
+++ app/assets/javascripts/devise_sessions_modal.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	$("#tragdor").click(function(){
 		$("#makeitso").addClass('say_welcome');
-		$(".say_welcome").append("<span id=welcome>welcome to whatsgood!</span>")
+		$(".say_welcome").append("<span id=welcome>whatsgood!</span>")
+		$( ".say_welcome" ).fadeOut( 3120, function() {
+    		// Animation complete.
+  		});
 	});
 });
